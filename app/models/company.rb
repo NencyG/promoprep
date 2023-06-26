e26a4be418a4b0fce98d8a37145adadcdc19b536
Nency <nencyg.tagline@gmail.com>
2023-06-26 13:01:00 +0530

    Add validation and Association

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
+# company modal
 class Company < ApplicationRecord
+  has_many :promos, dependent: :destroy
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i}
 end
--- app/models/promo.rb
+++ app/models/promo.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+
+# Promo modal
 class Promo < ApplicationRecord
-  belongs_to :companies
+  belongs_to :companies, class_name: 'Company'
+  validates :name, presence: true
+  validates :start_date, presence: true
+  validates :end_date , presence: true
+  validates :description, presence: true, length: { minimum: 5 }
 end

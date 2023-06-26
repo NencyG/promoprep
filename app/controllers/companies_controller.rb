4e9aaa82b6a48a3df084013839ec31aca69d4da0
Nency <nencyg.tagline@gmail.com>
2023-06-26 11:29:13 +0530

    Remove comments

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,70 +1,58 @@
-class CompaniesController < ApplicationController
-  before_action :set_company, only: %i[ show edit update destroy ]
+# frozen_string_literal: true
 
-  # GET /companies or /companies.json
+# Compaines controller
+class CompaniesController < ApplicationController
+  before_action :set_company, only: %i[show edit update destroy]
+
   def index
     @companies = Company.all
   end
 
-  # GET /companies/1 or /companies/1.json
-  def show
-  end
+  def show; end
 
-  # GET /companies/new
   def new
     @company = Company.new
   end
 
-  # GET /companies/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /companies or /companies.json
   def create
     @company = Company.new(company_params)
 
     respond_to do |format|
       if @company.save
-        format.html { redirect_to company_url(@company), notice: "Company was successfully created." }
-        format.json { render :show, status: :created, location: @company }
+        format.html { redirect_to company_url(@company), notice: 'Company was successfully created.' }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @company.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /companies/1 or /companies/1.json
   def update
     respond_to do |format|
       if @company.update(company_params)
-        format.html { redirect_to company_url(@company), notice: "Company was successfully updated." }
-        format.json { render :show, status: :ok, location: @company }
+        format.html { redirect_to company_url(@company), notice: 'Company was successfully updated.' }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @company.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /companies/1 or /companies/1.json
   def destroy
     @company.destroy
 
     respond_to do |format|
-      format.html { redirect_to companies_url, notice: "Company was successfully destroyed." }
-      format.json { head :no_content }
+      format.html { redirect_to companies_url, notice: 'Company was successfully destroyed.' }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_company
-      @company = Company.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def company_params
-      params.require(:company).permit(:name, :email)
-    end
+  def set_company
+    @company = Company.find(params[:id])
+  end
+
+  def company_params
+    params.require(:company).permit(:name, :email)
+  end
 end

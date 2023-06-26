0ee98e63fece3ab089453db961e263c800fc2826
Nency <nencyg.tagline@gmail.com>
2023-06-26 12:21:42 +0530

    Add validation for name and email

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,4 @@
 class Company < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i}
 end

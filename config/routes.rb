57fa00cb8cf0de2d15dd709571999cfa4b080b79
Nency <nencyg.tagline@gmail.com>
2023-06-27 17:58:12 +0530

    Add condition for link and routs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :promos
+  get 'promos/filter', to: 'promos#filter', as: 'promos_filter'
   resources :companies
   root 'home#index'
   devise_for :users

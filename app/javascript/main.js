146a2c29e4fc560d39af8cf87e515d443da70b6e
Nency <nencyg.tagline@gmail.com>
2023-06-29 12:33:42 +0530

    Add valiation

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -1,5 +1,5 @@
 $("#promo_company_id").on("change", function () {
-    var compyid = $(this).val();
-    let frame = document.querySelector("turbo-frame#promo-list");
-    frame.src = '/promos?company_id='+ compyid +'&format=turbo_stream'
+  var compyid = $(this).val();
+  let frame = document.querySelector("turbo-frame#promo-list");
+  frame.src = '/promos?company_id='+ compyid +'&format=turbo_stream'
 });

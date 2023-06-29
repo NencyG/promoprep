b76868eca450233caeb43a7a04a0db1a18edafa0
Nency <nencyg.tagline@gmail.com>
2023-06-29 15:19:45 +0530

    Add relashinship between two table

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,8 +4,9 @@
 class Company < ApplicationRecord
   belongs_to :user
   has_many :filter_options, dependent: :destroy, inverse_of: :company
+  has_many :company, dependent: :destroy
   accepts_nested_attributes_for :filter_options, allow_destroy: true, reject_if: :all_blank
-  has_many :promos, dependent: :destroy
+  has_many :promo_filter_options, dependent: :destroy
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true,
                     format: { with: /\A[\w+\-.]+@[a-z\d-]+(\.[a-z\d-]+)*\.[a-z]+\z/i }
--- app/models/filter_option.rb
+++ app/models/filter_option.rb
@@ -1,4 +1,5 @@
 class FilterOption < ApplicationRecord
   belongs_to :company
   validates :name, presence: true
+  has_many :promo_filter_options, dependent: :destroy
 end

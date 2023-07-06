d0681ed5f8a6573628cb914ec4b904edac2223a6
Nency <nencyg.tagline@gmail.com>
2023-07-06 17:57:28 +0530

    Add Validation in model

--- app/models/promo.rb
+++ app/models/promo.rb
@@ -5,8 +5,9 @@ class Promo < ApplicationRecord
   belongs_to :company
   has_many :promo_filter_options, dependent: :destroy
   has_many :filter_options, through: :promo_filter_options
-  validates :name, presence: true
-  validates :start_date, presence: true
-  validates :end_date, presence: true
+  validates_presence_of :name
+  validates_presence_of :start_date
+  validates_presence_of :end_date
   validates :description, presence: true, length: { minimum: 5 }
+  validates_presence_of :promo_filter_options
 end
--- app/models/promo_filter_option.rb
+++ app/models/promo_filter_option.rb
@@ -1,5 +1,4 @@
 class PromoFilterOption < ApplicationRecord
   belongs_to :promo
   belongs_to :filter_option
-  validates :filter_option_id, presence: true
 end

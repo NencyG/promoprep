d67c4603f16e6ef47cc81688913c0d2d61ef2665
Nency <nencyg.tagline@gmail.com>
2023-07-10 16:12:02 +0530

    Make custom route and update export button

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :promos
+  get 'export/:company_id', to: 'promos#export', as: 'export'
   resources :companies
   root 'home#index'
   devise_for :users

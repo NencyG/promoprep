937e899d386cce47a389e358a49e9b3f49b7fce4
Nency <nencyg.tagline@gmail.com>
2023-07-14 10:58:26 +0530

    Delete temp file after perform service

--- app/jobs/import_csv_job.rb
+++ app/jobs/import_csv_job.rb
@@ -1,7 +1,15 @@
 class ImportCsvJob < ApplicationJob
   queue_as :default
+  after_perform :delete_temp_file
 
   def perform(file_path)
+    @file_path = file_path
     CsvImportPromosService.new.call(file_path)
+  end
+
+  private
+
+  def delete_temp_file
+    File.delete(@file_path) if File.exist?(@file_path)
   end
 end

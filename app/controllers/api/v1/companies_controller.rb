8aa94b8022d36d74f334afb27756e8f8db9bc847
Nency <nencyg.tagline@gmail.com>
2023-07-19 13:01:52 +0530

    Change class name

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -1,19 +1,15 @@
-module Api
-  module V1
-    class CompaniesController < ApplicationController
-      def index
-        @companies = Company.all
-        render json: { status: 200, message: 'Fetched all the companies successfully', data: @companies }, status: :ok
-      end
+class Api::V1::CompaniesController < ApplicationController
+  def index
+    @companies = Company.all
+    render json: { status: 200, message: 'Fetched all the companies successfully', data: @companies }, status: :ok
+  end
 
-      def show
-        @companies = Company.find_by(id: params[:id])
-        if @companies
-          render json: { status: 200, message: 'Success', data: @companies }, status: :ok
-        else
-          render json: { error: 'Company Not Found', status: :bad_request }
-        end
-      end
+  def show
+    @companies = Company.find_by(id: params[:id])
+    if @companies
+      render json: { status: 200, message: 'Success', data: @companies }, status: :ok
+    else
+      render json: { error: 'Company Not Found', status: :bad_request }
     end
   end
 end

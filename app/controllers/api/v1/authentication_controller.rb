c47e1274a7c17471818c8da826be33983cb62d16
Nency <nencyg.tagline@gmail.com>
2023-07-28 10:35:21 +0530

    Ädded serializer in login api and changs in login api params

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -4,19 +4,14 @@ module Api::V1
     skip_before_action :authorize_request, only: :login
 
     def login
-      @user = User.find_by_email(params[:email])
-      if @user&.valid_password?(params[:password])
+      @user = User.find_by_email(params[:authentication][:email])
+      if @user&.valid_password?(params[:authentication][:password])
         token = JWT.encode({ user_id: @user.id }, Rails.application.secrets.secret_key_base, 'HS256')
-        response_200('Login Sucessfully', { token:, user: @user })
+        user = UserSerializer.new(@user).serializable_hash[:data]
+        response_200('Login Sucessfully', { token:, user: })
       else
         response_401('Invalid Email or Password')
       end
-    end
-
-    private
-
-    def login_params
-      params.require(:user).permit(:email, :password)
     end
   end
 end
